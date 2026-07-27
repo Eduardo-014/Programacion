c279ee5ecf0d8a4505e8f6cdfc9595986d248c2d
bonil <bonil@MOISES>
2026-07-26 18:52:11 -0600

    Practica para imprimir texto y guardar valores en variables. Ver tipos de variables, y los valores que almacenan.

--- Hello_Mundo/src/hello_mundo/Hello_Mundo.java
+++ Hello_Mundo/src/hello_mundo/Hello_Mundo.java
@@ -10,11 +10,64 @@ package hello_mundo;
  */
 public class Hello_Mundo {
 
-    /**
-     * @param args the command line arguments
-     */
+    //Doble plaqueta sirve para añadir un comentario en una sola linea
+    /*
+    Pleca + Asterisco -> Abrir bloque comentario
+    Asterisco + Pleca -> Cerrar el bloque comentario
+    
+    To lo que esta escrito dentro de estos simbolos es un comentario
+    */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        /*
+          1. Imprimir en pantalla 
+              a. Salidas formateadas
+              b. Caracteres de Escape
+          2. Camturar datos
+        */
+        
+        System.out.println("Hola, me llamo Moisés!");
+        System.out.println("Este sera mi promer programa");
+        System.out.println("PROGRAMAAAAA!!");
+        
+        System.out.print("Vamos camino a ");
+        System.out.print("la clase de ");
+        System.out.print("PROGRA!!");
+        
+        System.out.println("");
+         
+        System.out.printf("Tu apellido es %S/n", "Bonilla");
+        //System.out.println("");
+        //alt + 92 -> \
+        
+        System.out.printf("%S tu apellido es/n", "Bonilla");
+        //System.out.println("");
+        
+        System.out.printf("Tu apellido %S es/n", "Bonilla");
+        
+        System.out.println("Hola \n este es \n Un nuevo \n Amanecer");
+        System.out.println("Hola \t este es \t un nuevo\t amanecer");
     
-}
+        //Variables
+    int edad;       //Tipo entero
+    double peso;    //Tipo decimal  
+    char inicial;   //Tipo caracter -> Letras,digitos
+                    //,simbolos especiales
+    String nombre;  //Tipo cadena
+    boolean estado; //Tipo estado ->true(verdadero)
+                    //false(falso)
+                   
+    edad = 23;      
+    peso = 60.6;  
+    inicial = 'M'; //alt + 39-> '
+    nombre = "Moises"; 
+    
+     System.out.printf("Tengo %d años\n",edad);
+     System.out.printf("Mi peso ideal es %.2f\n",peso);
+     System.out.printf("Dime %c por mi inicial\n",inicial);
+     System.out.printf("Me llamo %s\n",nombre);
+    
+    }//Fin de main
+    
+}//Fin de Class
+    
